313e714113355b09ebe43a084ab49f210a732ded
Николай <raspaev.niko@mail.ru>
2023-03-29 19:10:21 +0400

    реализованны все модели бд

--- HospitalDatabaseImplement/Models/Patient.cs
+++ HospitalDatabaseImplement/Models/Patient.cs
@@ -1,6 +1,9 @@
-﻿using HospitalDataModels.Models;
+﻿using HospitalContracts.BindingModels;
+using HospitalContracts.ViewModels;
+using HospitalDataModels.Models;
 using System;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 
 namespace HospitalDatabaseImplement.Models;
 
@@ -21,4 +24,38 @@ public partial class Patient : IPatient
     public string? TelephoneNumber { get; set; }
 
     public virtual List<Contract> Contracts { get; } = new List<Contract>();
+
+    public static Patient Create(PatientBindingModel model)
+    {
+        return new Patient()
+        {
+            Id = model.Id,
+            Surname = model.Surname,
+            Name = model.Name,
+            Patronymic = model.Patronymic,
+            Birthdate = model.Birthdate,
+            Passport = model.Passport,
+            TelephoneNumber = model.TelephoneNumber,
+        };
+    }
+
+    public void Update(PatientBindingModel model)
+    {
+        Surname = model.Surname;
+        Name = model.Name;
+        Patronymic = model.Patronymic;
+        Birthdate = model.Birthdate;
+        Passport = model.Passport;
+        TelephoneNumber = model.TelephoneNumber;
+    }
+    public PatientViewModel GetViewModel => new()
+    {
+        Id = Id,
+        Surname = Surname,
+        Name = Name,
+        Patronymic = Patronymic,
+        Birthdate = Birthdate,
+        Passport = Passport,
+        TelephoneNumber = TelephoneNumber,
+    };
 }
