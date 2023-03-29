4065f11e163441ef302e4c9723076769941b3192
Николай <raspaev.niko@mail.ru>
2023-03-29 18:13:24 +0400

    изменение

--- HospitalDatabaseImplement/Models/AcademicRank.cs
+++ HospitalDatabaseImplement/Models/AcademicRank.cs
@@ -1,6 +1,8 @@
 ﻿using HospitalDataModels.Models;
 using System;
 using System.Collections.Generic;
+using HospitalContracts.BindingModels;
+using HospitalContracts.ViewModels;
 
 namespace HospitalDatabaseImplement.Models;
 
@@ -11,4 +13,41 @@ public partial class AcademicRank : IAcademicRank
     public string AcademicRankName { get; set; } = null!;
 
     public virtual List<Doctor> Doctors { get; } = new List<Doctor>();
+
+    public static AcademicRank? Create(AcademicRankBindingModel model)
+    {
+        if (model == null)
+        {
+            return null;
+        }
+        return new AcademicRank()
+        {
+            Id = model.Id,
+            AcademicRankName = model.AcademicRankName,
+        };
+    }
+
+    public static AcademicRank Create(AcademicRankViewModel model)
+    {
+        return new AcademicRank
+        {
+            Id = model.Id,
+            AcademicRankName = model.AcademicRankName,
+        };
+    }
+
+    public void Update(AcademicRankBindingModel model)
+    {
+        if (model == null)
+        {
+            return;
+        }
+        AcademicRankName = model.AcademicRankName;
+    }
+
+    public AcademicRankViewModel GetViewModel => new()
+    {
+        Id = Id,
+        AcademicRankName = AcademicRankName
+    };
 }
