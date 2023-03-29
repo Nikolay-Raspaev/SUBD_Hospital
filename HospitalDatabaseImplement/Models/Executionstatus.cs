8e32e4251ddb50907a63b0baadef35723fe2cb43
Николай <raspaev.niko@mail.ru>
2023-03-29 18:48:17 +0400

    fix

--- HospitalDatabaseImplement/Models/Executionstatus.cs
+++ HospitalDatabaseImplement/Models/Executionstatus.cs
@@ -1,6 +1,9 @@
-﻿using HospitalDataModels.Models;
+﻿using HospitalContracts.BindingModels;
+using HospitalContracts.ViewModels;
+using HospitalDataModels.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace HospitalDatabaseImplement.Models;
 
@@ -8,7 +11,44 @@ public partial class ExecutionStatus : IExecutionStatus
 {
     public int Id { get; set; }
 
-    public string ExecutionstatusName { get; set; } = null!;
+    public string ExecutionStatusName { get; set; } = null!;
 
     public virtual List<Contract> Contracts { get; } = new List<Contract>();
+
+    public static ExecutionStatus? Create(ExecutionStatusBindingModel model)
+    {
+        if (model == null)
+        {
+            return null;
+        }
+        return new ExecutionStatus()
+        {
+            Id = model.Id,
+            ExecutionStatusName = model.ExecutionStatusName,
+        };
+    }
+
+    public static ExecutionStatus Create(ExecutionStatusViewModel model)
+    {
+        return new ExecutionStatus
+        {
+            Id = model.Id,
+            ExecutionStatusName = model.ExecutionStatusName,
+        };
+    }
+
+    public void Update(ExecutionStatusBindingModel model)
+    {
+        if (model == null)
+        {
+            return;
+        }
+        ExecutionStatusName = model.ExecutionStatusName;
+    }
+
+    public ExecutionStatusViewModel GetViewModel => new()
+    {
+        Id = Id,
+        ExecutionStatusName = ExecutionStatusName,
+    };
 }
