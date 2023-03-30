3a22989280d17178fc2c723b27144ac8087d5656
Николай <raspaev.niko@mail.ru>
2023-03-30 19:37:58 +0400

    добавлен запуск через config

--- HospitalDatabaseImplement/HospitalBdContext.cs
+++ HospitalDatabaseImplement/HospitalBdContext.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using HospitalDatabaseImplement.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+using NLog.Internal;
 
 namespace HospitalDatabaseImplement;
 
@@ -34,10 +36,15 @@ public partial class HospitalBdContext : DbContext
 
     public virtual DbSet<ServicesJob> ServicesJobs { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Host=192.168.56.101;Port=5432;Database=SUBD;Username=postgres;Password=1234");
+//    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+//        => optionsBuilder.UseNpgsql("Host=192.168.56.101;Port=5432;Database=SUBD;Username=postgres;Password=1234");
 
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        string connectString = ConfigurationManager.AppSettings["connect"]!;
+        optionsBuilder.UseNpgsql(connectString);
+    }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<AcademicRank>(entity =>
